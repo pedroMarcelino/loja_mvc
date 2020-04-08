24dd38c3c5b96efec0a90643f54ea376539978b1
Marcelino <pedro.marcelinov@gmail.com>
2020-04-08 19:31:05 -0300

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    AULA MVC AA
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/avisos', function () {
+    return view('avisos');
+});
